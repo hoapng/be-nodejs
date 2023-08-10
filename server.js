fe68a3ee90065aefe2b3cc97d151b55c10468373
hoapng <giahoa01giathieu@gmail.com>
2023-08-10 18:55:59 +0700

    routes

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
-const app = express()
+const app = express()// app express
 const port = 8081
 
+//khai báo route
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+    res.send('<h1>Hello World!ABC</h1>')
 })
 
 app.listen(port, () => {
