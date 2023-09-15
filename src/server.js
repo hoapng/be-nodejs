f7236fcc84ce87d62139df52c04d594855eab361
hoapng <giahoa01giathieu@gmail.com>
2023-09-15 09:53:41 +0700

    MySQL

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const express = require('express')
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+// get the client
+const mysql = require('mysql2');
 
 const app = express()// app express
 const port = process.env.PORT || 8888
@@ -15,6 +17,24 @@ configViewEngine(app)
 
 //khai báo route
 app.use('/', webRoutes)
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'hoidanit',
+    password: '123456',
+    port: 3307
+});
+
+// simple query
+connection.query(
+    'SELECT * FROM Users',
+    function (err, results, fields) {
+        console.log("results", results); // results contains rows returned by server
+        console.log("fields", fields); // fields contains extra meta data about results, if available
+    }
+);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
