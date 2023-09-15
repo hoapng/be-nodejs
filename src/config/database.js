111ccb18cb5171088ea5f4e54453850e4cc42727
hoapng <giahoa01giathieu@gmail.com>
2023-09-15 22:59:44 +0700

    fix

--- src/config/database.js
+++ src/config/database.js
@@ -12,7 +12,6 @@ const connection = mysql.createPool({
     port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
     queueLimit: 0,
 });
 
--- src/server.js
+++ src/server.js
@@ -22,12 +22,12 @@ configViewEngine(app)
 app.use('/', webRoutes)
 
 // simple query
-connection.query(
-    'SELECT * FROM Users',
-    function (err, results, fields) {
-        // console.log("results", results); // results contains rows returned by server
-    }
-);
+// connection.query(
+//     'SELECT * FROM Users',
+//     function (err, results, fields) {
+//         // console.log("results", results); // results contains rows returned by server
+//     }
+// );
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
