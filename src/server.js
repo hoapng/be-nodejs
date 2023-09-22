27f95ccc0e921b50bcaabf4eed21f9664b02361a
hoapng <giahoa01giathieu@gmail.com>
2023-09-22 09:19:18 +0700

    create db

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
 const connection = require('./config/database')
+const mongoose = require('mongoose')
 
 const app = express()// app express
 const port = process.env.PORT || 8888
@@ -20,6 +21,13 @@ configViewEngine(app)
 
 //khai báo route
 app.use('/', webRoutes);
+
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+const Kitten = mongoose.model('Kitten', kittySchema);
+const cat = new Kitten({ name: 'Silence' });
+cat.save();
 
 (async () => {
     try {
