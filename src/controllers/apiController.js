e046dc6b419ff51ddce7088afa6ffb910c8eb1ca
hoapng <giahoa01giathieu@gmail.com>
2023-09-23 19:39:47 +0700

    upload multi Files

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -44,7 +44,7 @@ const deleteUserAPI = async (req, res) => {
     })
 }
 
-const postUploadSingleFile = async (req, res) => {
+const postUploadSingleFileApi = async (req, res) => {
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files were uploaded.');
     }
@@ -54,10 +54,32 @@ const postUploadSingleFile = async (req, res) => {
     return res.send('OK')
 }
 
+const postUploadMultipleFilesAPI = async (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    // console.log(req.files);
+    //upload single => files is an object
+    //upload multiple => files is an array
+    if (Array.isArray(req.files.image)) {
+        //upload multiple
+        let result = await uploadMultipleFiles(req.files.image);
+        return res.status(200).json({
+            EC: 0,
+            data: result
+        })
+
+    } else {
+        //upload single
+        return await postUploadSingleFileApi(req, res);
+    }
+}
+
 module.exports = {
     getUsersAPI,
     postCreateUserAPI,
     puttUpdateUserAPI,
     deleteUserAPI,
-    postUploadSingleFile
+    postUploadSingleFileApi,
+    postUploadMultipleFilesAPI
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getUsersAPI, postCreateUserAPI, puttUpdateUserAPI, deleteUserAPI, postUploadSingleFile } = require('../controllers/apiController');
+const { getUsersAPI, postCreateUserAPI, puttUpdateUserAPI, deleteUserAPI, postUploadSingleFileApi, postUploadMultipleFilesAPI } = require('../controllers/apiController');
 const routerAPI = express.Router()
 
 // router.METHOD('/route', hanler)
@@ -11,6 +11,8 @@ routerAPI.put('/users', puttUpdateUserAPI)
 
 routerAPI.delete('/users', deleteUserAPI)
 
-routerAPI.post('/file', postUploadSingleFile)
+routerAPI.post('/file', postUploadSingleFileApi)
+
+routerAPI.post('/files', postUploadMultipleFilesAPI)
 
 module.exports = routerAPI; //export default 
