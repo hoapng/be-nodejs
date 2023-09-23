1a1eaf4ba27a615e6addeb613ab1691ace461690
hoapng <giahoa01giathieu@gmail.com>
2023-09-23 13:16:25 +0700

    CRUD api

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -4,11 +4,49 @@ const getUsersAPI = async (req, res) => {
     let results = await User.find({});
 
     return res.status(200).json({
-        errorCode: 0,
+        EC: 0,
         data: results
     })
 }
 
+const postCreateUserAPI = async (req, res) => {
+
+    let { email, name, city } = req.body;
+
+    let user = await User.create({ email, name, city })
+
+    return res.status(200).json({
+        EC: 0,
+        data: user
+    })
+}
+
+const puttUpdateUserAPI = async (req, res) => {
+
+    let { email, name, city, userId } = req.body;
+
+    // console.log(email, name, city, req.body);
+
+    let user = await User.updateOne({ _id: userId }, { email: email, name: name, city: city })
+    return res.status(200).json({
+        EC: 0,
+        data: user
+    })
+}
+
+const deleteUserAPI = async (req, res) => {
+    const id = req.body.userId;
+    let results = await User.deleteOne({ _id: id });
+    return res.status(200).json({
+        EC: 0,
+        data: results
+    })
+}
+
+
 module.exports = {
-    getUsersAPI
+    getUsersAPI,
+    postCreateUserAPI,
+    puttUpdateUserAPI,
+    deleteUserAPI
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { getUsersAPI } = require('../controllers/apiController');
+const { getUsersAPI, postCreateUserAPI, puttUpdateUserAPI, deleteUserAPI } = require('../controllers/apiController');
 const routerAPI = express.Router()
 
 // router.METHOD('/route', hanler)
 routerAPI.get('/users', getUsersAPI)
+
+routerAPI.post('/users', postCreateUserAPI)
+
+routerAPI.put('/users', puttUpdateUserAPI)
+
+routerAPI.delete('/users', deleteUserAPI)
 
 
 module.exports = routerAPI; //export default 
