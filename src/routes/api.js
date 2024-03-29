65329d76fdf4601448ccbbad068f0a4898f99748
hoapng <giahoa01giathieu@gmail.com>
2024-03-29 22:32:38 +0700

    chua fix

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,34 +1,51 @@
-const express = require('express');
-const { getUsersAPI, postCreateUserAPI, puttUpdateUserAPI, deleteUserAPI, postUploadSingleFileApi, postUploadMultipleFilesAPI } = require('../controllers/apiController');
-const { postCreateCustomer, postCreateArrayCustomer, getAllCustomer, puttUpdateCustomer, deleteCustomer, deleteArrayCustomer } = require('../controllers/customerController');
-const routerAPI = express.Router()
+const express = require("express");
+const {
+  getUsersAPI,
+  postCreateUserAPI,
+  puttUpdateUserAPI,
+  deleteUserAPI,
+  postUploadSingleFileApi,
+  postUploadMultipleFilesAPI,
+} = require("../controllers/apiController");
+const {
+  postCreateCustomer,
+  postCreateArrayCustomer,
+  getAllCustomer,
+  puttUpdateCustomer,
+  deleteCustomer,
+  deleteArrayCustomer,
+} = require("../controllers/customerController");
+const { postCreateProject } = require("../controllers/projectController");
+const routerAPI = express.Router();
 
 // router.METHOD('/route', hanler)
-routerAPI.get('/users', getUsersAPI)
-routerAPI.post('/users', postCreateUserAPI)
-routerAPI.put('/users', puttUpdateUserAPI)
-routerAPI.delete('/users', deleteUserAPI)
+routerAPI.get("/users", getUsersAPI);
+routerAPI.post("/users", postCreateUserAPI);
+routerAPI.put("/users", puttUpdateUserAPI);
+routerAPI.delete("/users", deleteUserAPI);
 
-routerAPI.post('/file', postUploadSingleFileApi)
-routerAPI.post('/files', postUploadMultipleFilesAPI)
+routerAPI.post("/file", postUploadSingleFileApi);
+routerAPI.post("/files", postUploadMultipleFilesAPI);
 
-routerAPI.post('/customers', postCreateCustomer);
-routerAPI.post('/customers-many', postCreateArrayCustomer);
-routerAPI.get('/customers', getAllCustomer);
-routerAPI.put('/customers', puttUpdateCustomer);
-routerAPI.delete('/customers', deleteCustomer);
-routerAPI.delete('/customers-many', deleteArrayCustomer);
+routerAPI.post("/customers", postCreateCustomer);
+routerAPI.post("/customers-many", postCreateArrayCustomer);
+routerAPI.get("/customers", getAllCustomer);
+routerAPI.put("/customers", puttUpdateCustomer);
+routerAPI.delete("/customers", deleteCustomer);
+routerAPI.delete("/customers-many", deleteArrayCustomer);
 
-routerAPI.get('/info', (req, res) => {
-    return res.status(200).json({
-        data: req.query
-    })
+routerAPI.post("/projects", postCreateProject);
+
+routerAPI.get("/info", (req, res) => {
+  return res.status(200).json({
+    data: req.query,
+  });
 });
 
-routerAPI.get('/info/:name/:address', (req, res) => {
-    return res.status(200).json({
-        data: req.params
-    })
+routerAPI.get("/info/:name/:address", (req, res) => {
+  return res.status(200).json({
+    data: req.params,
+  });
 });
 
-module.exports = routerAPI; //export default 
+module.exports = routerAPI; //export default
