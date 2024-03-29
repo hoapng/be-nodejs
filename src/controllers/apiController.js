905eb89f4bed0dc4b2ea4c46dd0454c73adc9c72
hoapng <giahoa01giathieu@gmail.com>
2024-03-29 22:13:57 +0700

    fix

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,85 +1,88 @@
 const User = require("../models/user");
-const uploadSingleFile = require("../services/fileService");
+const {
+  uploadSingleFile,
+  uploadMultipleFiles,
+} = require("../services/fileService");
 
 const getUsersAPI = async (req, res) => {
-    let results = await User.find({});
+  let results = await User.find({});
 
-    return res.status(200).json({
-        EC: 0,
-        data: results
-    })
-}
+  return res.status(200).json({
+    EC: 0,
+    data: results,
+  });
+};
 
 const postCreateUserAPI = async (req, res) => {
+  let { email, name, city } = req.body;
 
-    let { email, name, city } = req.body;
+  let user = await User.create({ email, name, city });
 
-    let user = await User.create({ email, name, city })
-
-    return res.status(200).json({
-        EC: 0,
-        data: user
-    })
-}
+  return res.status(200).json({
+    EC: 0,
+    data: user,
+  });
+};
 
 const puttUpdateUserAPI = async (req, res) => {
+  let { email, name, city, userId } = req.body;
 
-    let { email, name, city, userId } = req.body;
+  // console.log(email, name, city, req.body);
 
-    // console.log(email, name, city, req.body);
-
-    let user = await User.updateOne({ _id: userId }, { email: email, name: name, city: city })
-    return res.status(200).json({
-        EC: 0,
-        data: user
-    })
-}
+  let user = await User.updateOne(
+    { _id: userId },
+    { email: email, name: name, city: city }
+  );
+  return res.status(200).json({
+    EC: 0,
+    data: user,
+  });
+};
 
 const deleteUserAPI = async (req, res) => {
-    const id = req.body.userId;
-    let results = await User.deleteOne({ _id: id });
-    return res.status(200).json({
-        EC: 0,
-        data: results
-    })
-}
+  const id = req.body.userId;
+  let results = await User.deleteOne({ _id: id });
+  return res.status(200).json({
+    EC: 0,
+    data: results,
+  });
+};
 
 const postUploadSingleFileApi = async (req, res) => {
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-    }
-    let results = await uploadSingleFile(req.files.image);
-    console.log("results", results)
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  let results = await uploadSingleFile(req.files.image);
+  console.log("results", results);
 
-    return res.send('OK')
-}
+  return res.send("OK");
+};
 
 const postUploadMultipleFilesAPI = async (req, res) => {
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-    }
-    // console.log(req.files);
-    //upload single => files is an object
-    //upload multiple => files is an array
-    if (Array.isArray(req.files.image)) {
-        //upload multiple
-        let result = await uploadMultipleFiles(req.files.image);
-        return res.status(200).json({
-            EC: 0,
-            data: result
-        })
-
-    } else {
-        //upload single
-        return await postUploadSingleFileApi(req, res);
-    }
-}
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  // console.log(req.files);
+  //upload single => files is an object
+  //upload multiple => files is an array
+  if (Array.isArray(req.files.image)) {
+    //upload multiple
+    let result = await uploadMultipleFiles(req.files.image);
+    return res.status(200).json({
+      EC: 0,
+      data: result,
+    });
+  } else {
+    //upload single
+    return await postUploadSingleFileApi(req, res);
+  }
+};
 
 module.exports = {
-    getUsersAPI,
-    postCreateUserAPI,
-    puttUpdateUserAPI,
-    deleteUserAPI,
-    postUploadSingleFileApi,
-    postUploadMultipleFilesAPI
-}
+  getUsersAPI,
+  postCreateUserAPI,
+  puttUpdateUserAPI,
+  deleteUserAPI,
+  postUploadSingleFileApi,
+  postUploadMultipleFilesAPI,
+};
