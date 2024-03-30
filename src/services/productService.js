b499e9838dcf593aa577d1a578d9dda5815cc773
hoapng <giahoa01giathieu@gmail.com>
2024-03-30 07:25:05 +0700

    add user to project

--- src/services/productService.js
+++ src/services/productService.js
@@ -6,5 +6,14 @@ module.exports = {
       let result = await Project.create(data);
       return result;
     }
+    if (data.type === "ADD-USERS") {
+      let myProject = await Project.findById(data.projectId).exec();
+      // console.log(myProject);
+      for (let i = 0; i < data.usersArr.length; i++) {
+        myProject.usersInfor.push(data.usersArr[i]);
+      }
+      let newResult = await myProject.save();
+      return newResult;
+    }
   },
 };
